23a6310e63cae28a0b2181a9913e9c7b0f523edd
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2016-04-04 18:17:17 +0200

    added setup.py for local installation
    Installation works, but at least in my newly created conda environments the
    dependencies could not be installed (matplotlib, numba), because some of their
    dependencies could not be installed from pypi. It worked after I installed
    dependencies manually with conda.

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='rtmodels',
+    version='0.1.0',
+    author='Sebastian Bitzer',
+    author_email='sebastian.bitzer@tu-dresden.de',
+    packages=['rtmodels'],
+    description='Models of response time.',
+    zip_safe=False, # needed, because otherwise numba cannot cache
+    install_requires=['NumPy >=1.7.0', 'matplotlib', 'numba'],
+    classifiers=[
+                'Development Status :: 3 - Alpha',
+                'Environment :: Console',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: BSD License',
+                'Programming Language :: Python :: 3',
+                'Topic :: Scientific/Engineering',
+                 ]
+)
