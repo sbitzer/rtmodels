7364aa9139dd8bf006520be588b5623ec7ef4fb2
Sebastian Bitzer <official@sbitzer.eu>
2017-10-18 16:29:27 +0200

    export rtmodel class for external use directly from importing the module

--- rtmodels/__init__.py
+++ rtmodels/__init__.py
@@ -2,4 +2,4 @@
 
 from .discrete_gauss import discrete_static_gauss, \
     sensory_discrete_static_gauss, extended_discrete_static_gauss
-from .rtmodel import estimate_abc_loglik, response_distance
+from .rtmodel import rtmodel, estimate_abc_loglik, response_distance
