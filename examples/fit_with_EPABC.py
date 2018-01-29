da8c5ee9d1a6363fe14de4dfcf8412c661dd1c7a
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2018-01-29 16:13:31 +0100

    update fit_with_EPABC to new pyEPABC parameter interface
    includes new, more informative plots and new trial-specific plot of accumulated
    evidences for posterior mode parameters

--- examples/fit_with_EPABC.py
+++ examples/fit_with_EPABC.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 """
+This script demonstrates basic properties of a discrete static Gauss model 
+together with inference using pyEPABC. The example neglects the most useful
+component of the model: the ability to include within-trial variations of 
+stimulus features into model predictions. Consequently, the discrete static 
+Gauss model here only implements a discretised drift-diffusion model (cf. 
+Bitzer et al., Frontiers in Human Neuroscience, 2014, 8, 
+https://doi.org/10.3389/fnhum.2014.00102).
+
 Created on Tue Mar 29 15:48:16 2016
 
 @author: Sebastian Bitzer (sebastian.bitzer@tu-dresden.de)
@@ -8,119 +16,114 @@ Created on Tue Mar 29 15:48:16 2016
 import os.path
 import numpy as np
 import pandas as pd
-import seaborn as sns
-from scipy.stats import norm
 import matplotlib.pyplot as plt
-from pyEPABC import run_EPABC
+import pyEPABC
+from pyEPABC.parameters import exponential, gaussprob
 import rtmodels
 
 
-def plot_param_dist(samples, parnames, axlim_q=1):
-    pg = sns.PairGrid(samples, hue='distribution', diag_sharey=False)
-    pg.map_diag(sns.kdeplot, shade=True)
-    pg.map_offdiag(plt.scatter, alpha=0.3)
+#%% load data
+data = pd.read_csv(os.path.join(os.path.dirname(__file__), 'responsedata.csv'))
+
+N = data.shape[0]
+
+
+#%% create response model with some standard parameter settings
+model = rtmodels.discrete_static_gauss(dt=0.05, maxrt=2.5, choices=[1, 2], 
+    Trials=data.stimulus.values, means=np.c_[[-25, 0], [25, 0]], 
+    intstd=70, noisestd=60)
+
+print(model)
+
+ch_sim, rt_sim = model.gen_response(np.arange(N), 1000)
+
+fig, axes = plt.subplots(2, 1, sharex=True, sharey=True)
+model.plot_response_distribution(data.choice, data.RT, ax=axes[0])
+model.plot_response_distribution(ch_sim, rt_sim, ax=axes[1])
+
+axes[0].set_ylabel('response data')
+axes[1].set_ylabel('model simulations')
+axes[1].set_xlabel('RT (s)')
+
+
+#%% define parameter prior
+pars = pyEPABC.parameters.parameter_container()
+
+pars.add_param('noisestd', np.log(50), 1.2, exponential())
+pars.add_param('bound', 0.1, 1, gaussprob(0.5, 0.5))
+pars.add_param('prior', 0, .5, gaussprob())
+pars.add_param('ndtmean', -1, 1)
+pars.add_param('ndtspread', np.log(.2), 1, exponential())
+pars.add_param('lapseprob', -1.65, 1, gaussprob()) # median approx at 0.05
+pars.add_param('lapsetoprob', 0, 1, gaussprob())
+
+pg = pars.plot_param_dist();
+pg.fig.tight_layout()
+
+
+#%% fit with EP-ABC
+simfun = lambda data, dind, parsamples: model.gen_distances_with_params(
+        data[0], data[1], dind, pars.transform(parsamples), pars.names)
+
+# maximum allowed difference in RT for accepting samples
+epsilon = 0.05
+
+ep_mean, ep_cov, ep_logml, nacc, ntotal, runtime = pyEPABC.run_EPABC(
+        np.c_[data.choice, data.RT], simfun, None, pars.mu, pars.cov, 
+        epsilon=epsilon, minacc=2000, samplestep=10000, samplemax=6000000, 
+        npass=2, alpha=0.5, veps=2 * epsilon)
+
+
+#%% investigate posterior
+modes = pars.get_transformed_mode(ep_mean, ep_cov)
+
+fig, axes = pars.compare_pdfs(ep_mean, ep_cov)
+
+ch_post, rt_post = model.gen_response_from_Gauss_posterior(
+        np.arange(N), pars.names, ep_mean, ep_cov, 500, pars.transform)
+
+fig, axes = plt.subplots(2, 1, sharex=True, sharey=True)
+model.plot_response_distribution(data.choice, data.RT, ax=axes[0])
+model.plot_response_distribution(ch_post, rt_post, ax=axes[1])
+
+axes[0].set_ylabel('response data')
+axes[1].set_ylabel('posterior predictive')
+axes[1].set_xlabel('RT (s)')
+
+
+#%% show single trial accumulated evidence trajectories
+
+# set model parameter values to mode of posterior
+for mode, name in zip(modes, pars.names):
+    setattr(model, name, mode)
     
-    # adjust axis limits distorted by kdeplot
-    for p, name in enumerate(parnames):
-        m = samples[name].quantile(axlim_q)
-        lims = [samples[name].min() - 0.1 * m, 1.1 * m]
-        pg.axes[0, p].set_xlim(lims)
-        if p == 0:
-            pg.axes[p, 1].set_ylim(lims)
+trials = np.r_[0, 4, 175]
+R = 5
+logpost, logliks = model.compute_logpost_from_features(trials, R)
+
+fig, axes = plt.subplots(3, 1, sharex=True, sharey=True)
+
+times = (np.arange(logpost.shape[0]) + 1) * model.dt
+
+for ax, tr in zip(axes, range(trials.size)):
+    ax.set_title('trial %d' % trials[tr])
+    for rep in range(R):
+        # find first time point at which bound is crossed
+        tind, cind = np.nonzero(logpost[:, :, tr, rep] > np.log(model.bound))
+        if tind.size > 0:
+            tind = tind[0]
         else:
-            pg.axes[p, 0].set_ylim(lims)
-            
-    pg.add_legend(frameon=True)
-    
-    return pg
-
-
-if __name__ == "__main__":
-    # load data
-    data = pd.read_csv(os.path.join(os.path.dirname(__file__), 'responsedata.csv'))
-    
-    parnames = ['ndtmean', 'ndtspread', 'noisestd', 'bound', 'prior', 
-                'lapseprob', 'lapsetoprob']
-
-    # number of samples to plot later
-    N = 500
-    
-    # initialise samples
-    samples = pd.DataFrame([], columns=(parnames+['distribution']))
-    
-    # implement constraints through transformations
-    # ndtmean is unconstrained, 
-    # ndtspread and noisestd must be positive, 
-    # bound must be in [0.5, 1]
-    # prior, lapseprob and lapsetoprob must be in [0, 1]
-    paramtransform = lambda params: np.c_[params[:, 0], 
-                                          np.exp(params[:, 1:3]), 
-                                          norm.cdf(params[:, 3] / 2 + 0.5), 
-                                          norm.cdf(params[:, 4:])]
-    
-    # these define the priors
-    prior_mean = np.array([-1, -1.5, 4, 0, 0, -1, 0])
-    prior_cov = np.diag(np.array([1, 1, 1.5, 1, 1, 1, 1]) ** 2)
-    
-    # sample from EPABC prior
-    samples_pr = np.random.multivariate_normal(prior_mean, prior_cov, N)
-    samples_pr = pd.DataFrame(paramtransform(samples_pr), columns=parnames)
-    samples_pr['distribution'] = 'epabc_prior'
-    samples = samples.append(samples_pr, ignore_index=True)
-    
-    # plot the prior(s)
-    pg_prior = plot_param_dist(samples[samples['distribution'].map(
-        lambda x: x.endswith('prior'))], parnames, axlim_q=0.98)
-    
-    # make model
-    model = rtmodels.discrete_static_gauss(maxrt=2.5, choices=[1, 2], 
-        Trials=data.stimulus.values, means=np.c_[[-25, 0], [25, 0]], 
-        intstd=70)
-    
-    # wrapper for sampling from model and directly computing distances
-    simfun = lambda data, dind, parsamples: model.gen_distances_with_params(
-        data[0], data[1], dind, paramtransform(parsamples), parnames)
-    distfun = None
-    
-    # maximum distance allowed for accepting samples, note that for 
-    # response_dist this is in units of RT
-    epsilon = 0.05
-    
-    # normalising constant of the uniform distribution defined by distfun and 
-    # epsilon; for response_dist this is: (2*epsilon for the Euclidean distance 
-    # between true and sampled RT and another factor of 2 for the two possible 
-    # responses - timed out responses don't occur here, because they are only a
-    # point mass, i.e., one particular value of response and RT)
-    veps = 2 * 2 * epsilon
-    
-    # run EPABC
-    ep_mean, ep_cov, ep_logml, nacc, ntotal, runtime = run_EPABC(
-        data.values[:, 1:], simfun, distfun, prior_mean, prior_cov, 
-        epsilon=epsilon, minacc=500, samplestep=10000, samplemax=2000000, 
-        npass=3, alpha=0.5, veps=veps)
-    
-    # sample from EPABC posterior
-    samples_pos = np.random.multivariate_normal(ep_mean, ep_cov, N)
-    samples_pos = pd.DataFrame(paramtransform(samples_pos), columns=parnames)
-    samples_pos['distribution'] = 'epabc_pos'
-    samples = samples.append(samples_pos, ignore_index=True)
+            tind = logpost.shape[0]
         
-    # see what happens when you ignore RTs, epco = ep choice only
-    simfun = lambda data, dind, parsamples: model.gen_distances_with_params(
-        data[0], np.nan, dind, paramtransform(parsamples), parnames)
-    epco_mean, epco_cov, epco_logml, epco_nacc, epco_ntotal, epco_runtime = run_EPABC(
-        data.values[:, 1:], simfun, distfun, prior_mean, prior_cov, 
-        epsilon=epsilon, minacc=10000, samplestep=10000, samplemax=2000000, 
-        npass=3, alpha=0.5, veps=0.5)
+        for alt in range(2):
+            ax.plot(times[:tind+1], np.exp(logpost[:tind+1, alt, tr, rep]), 
+                    color='C%d' % alt)
+            ax.plot(times[tind:], np.exp(logpost[tind:, alt, tr, rep]), ':',
+                    color='C%d' % alt)
     
-    # sample from EPABC choice only posterior
-    samples_posco = np.random.multivariate_normal(epco_mean, epco_cov, N)
-    samples_posco = pd.DataFrame(paramtransform(samples_posco), columns=parnames)
-    samples_posco['distribution'] = 'epabcco_pos'
-    samples = samples.append(samples_posco, ignore_index=True)
-    
-    # plot the posterior(s)
-    pg_pos = plot_param_dist(samples[samples['distribution'].map(
-        lambda x: x.endswith('pos'))], parnames)
-        
-    plt.show()
+xl = ax.get_xlim()
+for ax in axes:
+    ax.plot(xl, model.bound * np.ones(2), '--k', label='bound')
+    ax.set_ylabel('prob. correct')
+ax.set_xlim(xl)
+ax.set_xlabel('decision time (s)');
